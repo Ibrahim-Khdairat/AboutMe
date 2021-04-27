288bbc68b4a7846b5e03948b132770f98dba413e
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-04-27 00:06:39 -0700

    Class03

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+alert('hi');
+document.write('This game will test our relationship and how much you know about me !!');
+document.write('<br>');
+document.write('You have 5 Funny Questions about me . ');
+document.write('If you score 5 you are my best friend if you got Zero we should know each other ASAP :)');
+document.write('<br>');
+document.write('This game will test our relationship and how much you know about me !!');
+        document.write('<br>');
+        document.write('<br>');
+        document.write('You have 5 Funny Questions about me . ');
+        document.write('<br>');
+        document.write('<br>');
+        document.write('If you score 5 you are my best friend if you got Zero we should know each other ASAP :)')
+        document.write('<br>')
+        document.write('<br>')
+document.write('Does Ibrahim Love Cars ?');
+let ans1=confirm('Does Ibrahim Love Cars ? (yes or no)');
+
+document.write('Does Ibrahim Love "Mansaf" ?');
+let ans2=confirm('Does Ibrahim Love "Mansaf" ? (yes or no)');
+
+document.write('Does Ibrahim Love Sleeping ?');
+let ans3=confirm('Does Ibrahim Love Sleeping ? (yes or no)');
+
+document.write('Does Ibrahim Love Sports ?');
+let ans4=confirm('Does Ibrahim Love Sports ?(yes or no)');
+
+document.write('Does Ibrahim Love Waching Movies ?');
+let ans5=confirm('Does Ibrahim Love Waching Movies ? (yes or no)');
